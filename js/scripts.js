68be295da87eee3eb6108c23683fcc80fbe1a7cf
Glitch (nielit-chandigarh) <none>
2023-07-23 10:36:03 +0000

    🕵🏋 Checkpoint ./js/scripts.js:57629255/331

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
 // Show the "Go to Top" button when the user scrolls down
 window.addEventListener('scroll', function() {
   const scrollToTopButton = document.querySelector('.go-to-top');
