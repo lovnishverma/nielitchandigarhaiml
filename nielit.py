b361710eb9426f8daeef23db8f00a68a42b89afb
Glitch (nielit-chandigarh) <none>
2023-07-23 10:50:08 +0000

    🎹🚇 Checkpoint ./start.sh:57629255/20 ./requirements.txt:57629255/21 ./index.html:57629255/67283 ./nielit.py:57629255/754 ./templates/index.html:57629255/67440 ./css/styles.css:57629255/240285 ./static/css/styles.css:57629255/240285 ./js/scripts.js:57629255/316 ./static/js/scripts.js:57629255/316

---
+++ nielit.py
@@ -0,0 +1,28 @@
+from flask import *
+from datetime import datetime, timedelta
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Get the current UTC time
+    utc_now = datetime.utcnow()
+
+    # Define the timezone for India (IST)
+    tz = pytz.timezone('Asia/Kolkata')
+
+    # Get the UTC offset for the India time zone
+    india_offset = timedelta(seconds=tz.utcoffset(utc_now).total_seconds())
+
+    # Add the UTC offset to the current UTC time to get India time
+    india_time = utc_now + india_offset
+
+    # Extract date and time components in 12-hour format
+    date = india_time.strftime("%Y-%m-%d")
+    time = india_time.strftime("%I:%M %p")
+
+    return render_template("index.html", date=date, time=time)
+
+if __name__ == '__main__':
+    app.run()
---
+++ start.sh
@@ -0,0 +1,1 @@
+python nielit.py
