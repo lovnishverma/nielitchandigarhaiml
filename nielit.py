38a6cb8c4d53907bd481ec32d63bb67f40b041bb
Glitch (nielit-chandigarh) <none>
2023-07-24 17:45:14 +0000

    🍜🌦 Checkpoint ./templates/main.html:57629255/1468 ./templates/admin.html:57629255/1923 ./nielit.py:57629255/25015 ./requirements.txt:57629255/4

--- nielit.py
+++ nielit.py
@@ -14,7 +14,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-
 # User model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,68 +24,52 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return "<User {}>".format(self.username)
 
-
 # Load user function required by Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# Topic model for the database table
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    topic_image = db.Column(db.String(200), nullable=False)
+    topic_name = db.Column(db.String(100), nullable=False)
+    topic_details = db.Column(db.Text, nullable=False)
+    pdf_link = db.Column(db.String(200), nullable=False)
 
-# Create the database tables
+# Create the database and the tables
 db.create_all()
 
+# Admin Page - Add New Topic
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_page():
+    if request.method == 'POST':
+        if current_user.is_authenticated and current_user.username == "admin":
+            topic_image = request.form['topicImage']
+            topic_name = request.form['topicName']
+            topic_details = request.form['topicDetails']
+            pdf_link = request.form['pdfLink']
 
+            # Store the topic data in the database
+            new_topic = Topic(
+                topic_image=topic_image,
+                topic_name=topic_name,
+                topic_details=topic_details,
+                pdf_link=pdf_link
+            )
+            db.session.add(new_topic)
+            db.session.commit()
+
+            return redirect(url_for('main_page'))
+
+    return render_template('admin.html')
+
+# Main Website Page
 @app.route('/')
-@login_required
-def dashboard():
-    # Helper function to read and update visitor count
-    def get_visitor_count():
-        with open("visitor_count.txt", "r") as f:
-            count = int(f.read())
-        count += 1
-        with open("visitor_count.txt", "w") as f:
-            f.write(str(count))
-        return count
-
-    # Get the current UTC time
-    utc_now = datetime.utcnow()
-
-    # Define the timezone for India (IST)
-    tz = pytz.timezone('Asia/Kolkata')
-
-    # Get the UTC offset for the India time zone
-    india_offset = timedelta(seconds=tz.utcoffset(utc_now).total_seconds())
-
-    # Add the UTC offset to the current UTC time to get India time
-    india_time = utc_now + india_offset
-
-    # Extract date and time components in 12-hour format
-    date = india_time.strftime("%Y-%m-%d")
-    time = india_time.strftime("%I:%M %p")
-
-    # Extract only the year from the date
-    year = india_time.strftime("%Y")
-
-    # Get the current hour in the India time zone
-    india_hour = india_time.hour
-
-    # Determine the time of day based on the current hour
-    if 5 <= india_hour < 12:
-        time_of_day = 'Morning'
-    elif 12 <= india_hour < 17:
-        time_of_day = 'Afternoon'
-    elif 17 <= india_hour < 21:
-        time_of_day = 'Evening'
-    else:
-        time_of_day = 'Night'
-
-    # Get the visitor count
-    visitor_count = get_visitor_count()
-    username = current_user.username  # Get the username of the current user
-
-    return render_template("main.html", username=username, time_of_day=time_of_day, date=date, time=time, year=year,
-                           visitor_count=visitor_count)
-
+def main_page():
+    # Fetch all topics from the database
+    topics = Topic.query.all()
+    return render_template('main.html', topics=topics)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -103,7 +86,6 @@ def login():
         flash('Invalid username or password.', 'error')
 
     return render_template('login.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -130,6 +112,12 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully.', 'success')
+    return redirect(url_for('login'))
 
 @app.route('/users')
 @login_required
@@ -138,9 +126,8 @@ def list_users():
         users = User.query.all()
         return render_template('user_list.html', users=users)
     else:
-        flash("You do not have permission to access Admin page.", 'error')
+        flash("You do not have permission to access the Admin page.", 'error')
         return redirect(url_for('dashboard'))
-
 
 @app.route('/delete_user/<int:user_id>', methods=['POST'])
 @login_required
@@ -159,62 +146,6 @@ def delete_user(user_id):
     else:
         flash("You do not have permission to perform this action.", 'error')
     return redirect(url_for('list_users'))
-
-
-# Topic model for the database table
-class Topic(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    topic_image = db.Column(db.String(200), nullable=False)
-    topic_name = db.Column(db.String(100), nullable=False)
-    topic_details = db.Column(db.Text, nullable=False)
-    pdf_link = db.Column(db.String(200), nullable=False)
-
-
-# Admin Page - Add New Topic
-@app.route('/admin', methods=['GET', 'POST'])
-@login_required  # Require user to be logged in
-def admin_page():
-    if current_user.username == "admin":
-        if request.method == 'POST':
-            topic_image = request.form['topicImage']
-            topic_name = request.form['topicName']
-            topic_details = request.form['topicDetails']
-            pdf_link = request.form['pdfLink']
-
-            # Store the topic data in the database
-            new_topic = Topic(
-                topic_image=topic_image,
-                topic_name=topic_name,
-                topic_details=topic_details,
-                pdf_link=pdf_link
-            )
-            db.session.add(new_topic)
-            db.session.commit()
-
-            flash('Topic added successfully.', 'success')
-            return redirect(url_for('main_page'))
-
-        return render_template('add_topic.html')
-
-    flash('You do not have permission to access the Admin page.', 'error')
-    return redirect(url_for('dashboard'))
-
-
-# Main Website Page
-@app.route('/')
-def main_page():
-    # Fetch all topics from the database
-    topics = Topic.query.all()
-    return render_template('main.html', topics=topics)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('Logged out successfully.', 'success')
-    return redirect(url_for('login'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
